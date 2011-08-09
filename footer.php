1f76965dd2fdaed5c4db0c38994c5a7c1d42b5bf
hq <hq@ontapp.net>
2011-08-09 10:42:10 -0500

    try two

---
+++ footer.php
@@ -0,0 +1,15 @@
+			<footer>
+				<ul>
+					<li><?php echo '$mvp' ?>
+					<li><a href="#">contact us</a></li>
+					<li><a href="#">about onTapp</a></li>
+					<li><a href="#">restaurant signup</a></li>
+					<li><a href="#">terms of service</a></li>
+					<li><a href="#">profile</a></li>
+					<li>Copyright onTapp 2011</li>
+				</ul>
+			</footer>
+		</div>
+	</body>
+	<!--code to get MVP from database, based on user_id and number of orders and 		   a little randomness-->
+</html>
---
+++ restaurant.php
@@ -0,0 +1,9 @@
+	<?php require_once("header.php"); ?>
+		<div id="menu">
+		<div id="cart">
+		<div id="deals">
+		
+		</div>
+		</div>
+		</div>
+	<?php require_once("footer.php"); ?>
