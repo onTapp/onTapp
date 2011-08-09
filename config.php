650e0479b8a5b2abbe0934ed7d18660dc7ac7a81
hq <hq@ontapp.net>
2011-08-09 11:44:58 -0500

    Z try 3 12

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost = "localhost:80/ontapp";
-$dbuser = "root";
-$dbpassword = "root";
-$dbdatabase = "ontapp";
-
-$authors = "Zack Reneau-Wedeen and Rafi Khan";
-$config_basedir = "http://localhost:80/ontapp/";
-?>
--- restaurant.php
+++
@@ -1,9 +0,0 @@
-	<?php require_once("header.php"); ?>
-		<div id="menu">
-		<div id="cart">
-		<div id="deals">
-		
-		</div>
-		</div>
-		</div>
-	<?php require_once("footer.php"); ?>
